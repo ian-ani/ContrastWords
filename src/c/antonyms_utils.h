a8195c30269be2ff2a77fb4a8d505349d72bedce
Iris <irisaunav@gmail.com>
2026-03-16 23:05:37 +0100

    Fix headers with parameters as void

--- src/c/antonyms_utils.h
+++ src/c/antonyms_utils.h
@@ -6,10 +6,10 @@
 #include <string.h>
 
 // Inicializar y guardar el JSON en la variable global
-void init_json(const char *filename);
+void init_json(void);
 
 // Cerrar json
-void free_json();
+void free_json(void);
 
 // Abre el archivo
 FILE *open_file(const char *filename);
@@ -24,10 +24,10 @@ char *read_file(FILE *input_file);
 cJSON *parse_json(char *buffer);
 
 // Cuenta el numero de claves existentes en el archivo
-int get_number_keys();
+int get_number_keys(void);
 
 // Obtiene una clave aleatoria para que el usuario diga un antonimo de esta
-char *get_random_key();
+char *get_random_key(void);
 
 // Obtiene valores del diccionario en base a su clave
 char **get_values(char *word, int *count);
